a4d6abfd16cb1fa22f4c55ac1d90c2d031424c56
TaejungKim <tae0803@naver.com>
2024-10-25 21:00:50 +0900

    #5 [Feat] : 추가 movie list 컴포넌트
    - Movie List 컴포넌트 작성

---
+++ app/src/main/java/org/sopt/and/presentation/home/Component/MovieList.kt
@@ -0,0 +1,35 @@
+package org.sopt.and.presentation.home.Component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import org.sopt.and.R
+import org.sopt.and.presentation.home.MovieData
+import org.sopt.and.ui.theme.Typography
+
+@Composable
+fun MovieList(title:String, movieData: List<MovieData>){
+    Text(
+        text = title,
+        style = Typography.bodyLarge,
+        color = Color.White
+    )
+    LazyRow(
+        modifier = Modifier.padding(4.dp),
+        horizontalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        items(movieData.size){ index ->
+            if(title == stringResource(R.string.home_trust_editor))
+                MovieEditorItem(movieData[index])
+            else
+                MovieTopItem(movieData[index])
+
+        }
+    }
+}
---
+++ app/src/main/java/org/sopt/and/presentation/home/Component/MovieTopItem.kt
@@ -0,0 +1,50 @@
+package org.sopt.and.presentation.home.Component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.sopt.and.presentation.home.MovieData
+import org.sopt.and.ui.theme.Typography
+
+@Composable
+fun MovieTopItem(movieData: MovieData) {
+    Box {
+        // corner radius 설정
+        Card(
+            shape = RoundedCornerShape(5.dp),
+        ) {
+            Image(
+                painter = painterResource(movieData.img),
+                contentDescription = "영화 이미지",
+                modifier = Modifier
+                    .width(240.dp)
+                    .aspectRatio(0.7f),
+                contentScale = ContentScale.Crop
+            )
+        }
+        Text(
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .offset(y = 40.dp)
+                .padding(start = 10.dp),
+            text = movieData.title,
+            style = Typography.titleLarge,
+            fontSize = 80.sp,
+            color = Color.White
+        )
+    }
+}
