de1ef6046b96661c7a20f9ec23234b74a2d3e6b1
TaejungKim <tae0803@naver.com>
2024-10-25 17:36:20 +0900

    #5 [Fix] : Screen Route와 BottmNav Route를 일치
    - route 불일치 오류를 수정하기 위해 Screen.route로 변수명 변경

--- app/src/main/java/org/sopt/and/presentation/main/BottomNavigationItem.kt
+++ app/src/main/java/org/sopt/and/presentation/main/BottomNavigationItem.kt
@@ -2,6 +2,7 @@ package org.sopt.and.presentation.main
 
 import androidx.compose.ui.graphics.Color
 import org.sopt.and.R
+import org.sopt.and.presentation.navigation.Screen
 
 // 바텀 네비게이션 뷰에 필요한 데이터 객체 정의
 sealed class BottomNavItem(
@@ -11,7 +12,8 @@ sealed class BottomNavItem(
     val selectedColor: Color,
     val unselectedColor: Color
 ) {
-    object Home : BottomNavItem("Home", R.drawable.ic_home, "Home", Color.White, Color.Gray)
-    object Search : BottomNavItem("Search", R.drawable.ic_search, "Search", Color.White, Color.Gray)
-    object My : BottomNavItem("My", R.drawable.ic_my, "My", Color.White, Color.Gray)
+    // route를 Screen route와 일치시키게 변경
+    object Home : BottomNavItem(Screen.Home.route, R.drawable.ic_home, "Home", Color.White, Color.Gray)
+    object Search : BottomNavItem(Screen.Search.route, R.drawable.ic_search, "Search", Color.White, Color.Gray)
+    object My : BottomNavItem(Screen.MyPage.route, R.drawable.ic_my, "My", Color.White, Color.Gray)
 }
