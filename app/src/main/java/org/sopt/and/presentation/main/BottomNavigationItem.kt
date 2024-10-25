1533d7abdfe0a4e42b380aca6b0951821816f318
TaejungKim <tae0803@naver.com>
2024-10-25 17:20:52 +0900

    #5 [Refactor] : SA 패턴을 위한 MainScreen 변경
    - SA 패턴을 위해 기존 MainActivity에서 MainScreen으로 변경
    - MainScreen에 하단 바텀 navigation으로 정의

--- app/src/main/java/org/sopt/and/presentation/main/BottomNavigationItem.kt
+++ app/src/main/java/org/sopt/and/presentation/main/BottomNavigationItem.kt
@@ -1,8 +1,17 @@
 package org.sopt.and.presentation.main
 
+import androidx.compose.ui.graphics.Color
+import org.sopt.and.R
+
 // 바텀 네비게이션 뷰에 필요한 데이터 객체 정의
-data class BottomNavigationItem(
-    val label: Int,
-    val icon: Int,
-    val route: String
-)
+sealed class BottomNavItem(
+    val route: String,
+    val iconResource: Int,
+    val label: String,
+    val selectedColor: Color,
+    val unselectedColor: Color
+) {
+    object Home : BottomNavItem("Home", R.drawable.ic_home, "Home", Color.White, Color.Gray)
+    object Search : BottomNavItem("Search", R.drawable.ic_search, "Search", Color.White, Color.Gray)
+    object My : BottomNavItem("My", R.drawable.ic_my, "My", Color.White, Color.Gray)
+}
