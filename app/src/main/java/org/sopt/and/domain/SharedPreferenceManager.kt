a7669eaa98f20a5e6b6081069c7d3001137a0835
TaejungKim <tae0803@naver.com>
2024-10-31 09:06:17 +0900

    #5 [Refactor] : 수정 SharedPreference 초기화
    - initialize 반복 호출 방지를 위해 이미 초기화 된 경우 return으로 수정

--- app/src/main/java/org/sopt/and/domain/SharedPreferenceManager.kt
+++ app/src/main/java/org/sopt/and/domain/SharedPreferenceManager.kt
@@ -7,6 +7,10 @@ object SharedPreferenceManager {
     private lateinit var sharedPreferences: SharedPreferences
 
     fun initialize(context: Context) {
+        // 이미 초기화된 경우에는 초기화를 건너뛰게 설정
+        if(::sharedPreferences.isInitialized){
+            return
+        }
         sharedPreferences = context.getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
     }
 
