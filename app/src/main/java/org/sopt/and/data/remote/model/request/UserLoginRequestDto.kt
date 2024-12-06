d2f92e4b43bc777b1f2ce0524cc262464ccf7af3
TaejungKim <tae0803@naver.com>
2024-12-06 22:23:20 +0900

    #11 [feat] : repository 각 단의 model 생성

---
+++ app/src/main/java/org/sopt/and/data/remote/model/request/UserLoginRequestDto.kt
@@ -0,0 +1,12 @@
+package org.sopt.and.data.remote.model.request
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UserLoginRequestDto(
+    @SerialName("username")
+    val username: String,
+    @SerialName("password")
+    val password: String,
+)
---
+++ app/src/main/java/org/sopt/and/domain/model/UserLoginResponse.kt
@@ -0,0 +1,5 @@
+package org.sopt.and.domain.model
+
+data class UserLoginResponse (
+    val token: String
+)
